283a1fc1b91a50ca18e12a4d99b92fdd7c72d14e
Aaron Vichery <pa84igb@eduvaud.ch>
2026-03-25 12:10:19 +0100

    feat(auth):Ajouter la sécurité pour les 'OR 1=1 --'

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,5 +1,6 @@
 const db = require('../config/db');
 
+
 module.exports = {
 
     // ----------------------------------------------------------
@@ -9,12 +10,12 @@ module.exports = {
         const { email, password } = req.body;
 
         if (!email || !password) {
-            return res.status(400).json({ error: 'Email et mot de passe requis' });
+            return res.status(400).json({ error: 'Email et mot de passe requis'});
         }
 
-        const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
+        const query = `SELECT * FROM users WHERE email = ? AND password = ?`;
 
-        db.query(query, (err, results) => {
+        db.query(query, [email, password], (err, results) => {
             if (err) {
                 return res.status(500).json({ error: err.message, query: query });
             }
@@ -38,20 +39,20 @@ module.exports = {
             return res.status(400).json({ error: 'Email et mot de passe requis' });
         }
 
-        const query = `SELECT * FROM users WHERE email = '${email}'`;
+        const query = `SELECT * FROM users WHERE email = ? OR username = ?`;
 
-        db.query(query, (err, results) => {
+        db.query(query, [email, username], (err, results) => {
             if (err) {
                 return res.status(500).json({ error: err.message, query: query });
             }
 
             if (results.length > 0) {
-                return res.status(401).json({ error: 'Email déjà utilisé' });
+                return res.status(401).json({ error: `Email ou nom d'utilisateur déjà utilisé` });
             }
 
             if (results.length === 0) {
-                const search = `INSERT INTO users (username, email, password, role, address, photo_path) VALUES ('${username}', '${email}', '${password}', 'user', '${fulladdress}', NULL)`;
-                db.query(search, (err, result) => {
+                const search = `INSERT INTO users (username, email, password, role, address, photo_path) VALUES (?, ?, ?, ?, ?, ?)`;
+                db.query(search, [username, email, password, 'user', address, null ] ,(err, result) => {
                     if (err) {
                         console.error("Erreur d'inscription :", err.message);
                         return res.status(500).send("Erreur lors de la création du compte.");
